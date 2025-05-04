64028b6089dd22285da2adefb02540cfc92fac48
Francisco Carlos Pierna <pierna_f@hotmail.com>
2025-05-04 07:51:38 -0300

    imagem python excel 04/05/2025

---
+++ python_excel/imagem_python_paraexcel.py
@@ -0,0 +1,22 @@
+#Importando Imagem do computador para o Excel
+
+import xlsxwriter as opcoesDoXlsxWriter
+import os
+
+nomeCaminhoArquivo = 'C:\\python_projetos\\python_rpa_projetos\\python_excel\\ArquivoImagem.xlsx'
+workbook = opcoesDoXlsxWriter.Workbook(nomeCaminhoArquivo)
+sheetPadrao = workbook.add_worksheet("Dados")
+
+#Adicionando dados na Sheet
+sheetPadrao.write("B3", "Imagem logo Udemy")
+
+sheetPadrao.insert_image('B5', 'C:\\Users\\Aluno\\Desktop\\Curso RPA\\xlsxwriter\\logo.png')
+
+
+
+#Fechando o arquivo
+workbook.close()
+
+
+#Abrindo o arquivo
+os.startfile(nomeCaminhoArquivo)
