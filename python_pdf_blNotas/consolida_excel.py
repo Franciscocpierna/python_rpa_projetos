8e4fd61dc1a3905f7d2d8ffb53aa218aee52bb96
Francisco Carlos Pierna <pierna_f@hotmail.com>
2025-05-25 19:33:13 -0300

    consolida_excel  25/05/2025

--- python_pdf_blNotas/consolida_excel.py
+++ python_pdf_blNotas/consolida_excel.py
@@ -1,84 +1,57 @@
 import pandas as opcoesPandas
 import os
-
+ 
 arquivoTextoConsolidado = open("C:\\python_projetos\\python_rpa_projetos\\python_pdf_blNotas\\consolidar\\Arquivo_Exercicio_Bloco_Notas_Consolidado.txt", "w")
-
-#Caminho onde estão os arquivos
+ 
 caminhoArquivos = "C:\\python_projetos\\python_rpa_projetos\\python_pdf_blNotas\\consolidar"
-
-#Variável onde estão todos os arquivos
+ 
 listaArquivos = os.listdir(caminhoArquivos)
-
-#print(listaArquivos)
-
-listaCaminhoEArquivoBlocoNotas = [caminhoArquivos + '\\' + arquivo for arquivo in listaArquivos if arquivo[-3:] == 'txt']
-
-#print(listaCaminhoEArquivoBlocoNotas)
-
-#Criando os DataFrame para trabalhar com os Dados
-dadosArquivo = opcoesPandas.DataFrame()
-
-#Titulo
+ 
+# Lista somente arquivos .txt que não contenham 'Consolidado' no nome
+listaCaminhoEArquivoBlocoNotas = [
+    os.path.join(caminhoArquivos, arquivo)
+    for arquivo in listaArquivos
+    if arquivo.endswith('.txt') and 'Consolidado' not in arquivo
+]
+ 
+# Escreve o cabeçalho no arquivo consolidado
 arquivoTextoConsolidado.write('Linha;Escola;ID;Aluno;Primeiro Nome;Sobrenome;CPF;Idade\n')
-
-#Copiando todos os dados dos arquivos em dadosArquivo
+ 
 for arquivo in listaCaminhoEArquivoBlocoNotas:
-    
-    #Pego as informações de cada bloco de notas e armazeno como um DF em Dados
-    #dados = opcoesPandas.read_csv(arquivo)
-    dados = opcoesPandas.read_csv(arquivo, delimiter=';')
-    
-    #Percorrer todo o arquivo a partir da segunda linha
-    for pulaTitulo, linha in dados.iterrows():
-        
-        #Quebramos o texto que estava tudo na mesma linha e separamos em linhas
-        #['ABC;782707;Aline Rosa Ferreira;CPF: 12345678910;36']
-        linha = [item.split(';', 0)[0] for item in linha]
-        
-        #Resposta da segunda questão
-        #Separando a linha em colunas atravé do delimitador ;
-        separaLinhasEmColunas = linha[0].split(';')
-        
-        escola = separaLinhasEmColunas[0]
-        idAluno = separaLinhasEmColunas[1]
-        aluno = separaLinhasEmColunas[2]
-        cpf = separaLinhasEmColunas[3]
-        idade = separaLinhasEmColunas[4]
-        
-        #Resposta da terceira questão
-        #split quebra o nome em colunas através do espaço em branco
-        #pego a posição 0 que é a primeira coluna
-        primeiroNome = aluno.split(' ')[0] #Quebro em coluna e pego a coluna 0
-        
-        #split quebra o nome em colunas através do espaço em branco
-        #pego a posição última coluna
-        sobrenome = aluno.split(' ')[-1] #Quebro em coluna e pego a última coluna
-        
-        #Resposta da quarta questão
-        #CPF: 12345678910
-        #Pegando partes do CPF que veio dos arquivo de texto do bloco de notas
-        cpf_parte1 = cpf[5:8] #três números a partir da 5 posição
-        cpf_parte2 = cpf[8:11] #três números a partir da 8 posição
-        cpf_parte3 = cpf[11:14] #três números a partir da 11 posição
-        cpf_parte4 = cpf[14:16] #dois números a partir da 14 posição
-        
-        #No CPF estou juntando as partes e formando o CPF com os pontos e traço
-        cpf = str(cpf_parte1) + "." + str(cpf_parte2) + "." + str(cpf_parte3) + "-" + str(cpf_parte4)
-              
-        #Salvo os dados em formato que o pandas reconhece para salvar no Excel
-        #Salvo no Bloco de Notas
-        arquivoTextoConsolidado.write(';' + escola + ';' + idAluno + ';' + aluno + ';' + primeiroNome + ';' + sobrenome + ';' + cpf + ';' + idade + '\n')
-        
-
-#Fecho o bloco de notas que tem todos os dados consolidados
-arquivoTextoConsolidado.close() 
-
-idiomaParaAcentosDoArquivo = 'cp1252'
-
-#Abro o arquivo do bloco de notas como csv
-lerArquivoBlocoNotasConsolidado = opcoesPandas.read_csv("C:\\python_projetos\\python_rpa_projetos\\python_pdf_blNotas\\consolidar\\Arquivo_Exercicio_Bloco_Notas_Consolidado.txt", encoding=idiomaParaAcentosDoArquivo)
-
-#Criando uma nova planilha e passando os dados dos arquivos
-#dadosArquivo.to_excel("A:\\Python RPA\\Arquivo Texto e PDF\\Arquivo Consolidado.xlsx")
-lerArquivoBlocoNotasConsolidado.to_csv('C:\\python_projetos\\python_rpa_projetos\\python_pdf_blNotas\\consolidar\\Arquivo Final Exercicio Texto.csv', encoding='utf-8-sig')
-
+    try:
+        dados = opcoesPandas.read_csv(arquivo, encoding='cp1252', sep=';', header=0)
+ 
+        for _, linha in dados.iterrows():
+            escola = str(linha['Escola'])
+            idAluno = str(linha['ID'])
+            aluno = str(linha['Aluno'])
+            cpf = str(linha['CPF'])
+            idade = str(linha['Idade'])
+ 
+            primeiroNome = aluno.split(' ')[0]
+            sobrenome = aluno.split(' ')[-1]
+ 
+            # Ex: CPF: 12345678910
+            cpf_num = cpf.replace("CPF: ", "")
+            cpf_formatado = f"{cpf_num[:3]}.{cpf_num[3:6]}.{cpf_num[6:9]}-{cpf_num[9:11]}"
+ 
+            linha_final = f";{escola};{idAluno};{aluno};{primeiroNome};{sobrenome};{cpf_formatado};{idade}\n"
+            arquivoTextoConsolidado.write(linha_final)
+ 
+    except Exception as e:
+        print(f"Erro ao processar o arquivo {arquivo}: {e}")
+ 
+arquivoTextoConsolidado.close()
+ 
+# Agora converte para CSV
+lerArquivoBlocoNotasConsolidado = opcoesPandas.read_csv(
+    "C:\\python_projetos\\python_rpa_projetos\\python_pdf_blNotas\\consolidar\\Arquivo_Exercicio_Bloco_Notas_Consolidado.txt",
+    encoding='cp1252',
+    sep=';'
+)
+ 
+lerArquivoBlocoNotasConsolidado.to_csv(
+    'C:\\python_projetos\\python_rpa_projetos\\python_pdf_blNotas\\consolidar\\Arquivo Final Exercicio Texto.csv',
+    encoding='utf-8-sig',
+    index=False
+)
