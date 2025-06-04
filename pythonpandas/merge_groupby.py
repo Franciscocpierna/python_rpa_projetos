d97391147ce4859f29a76d8b249a0aab2a0d6d24
Francisco Carlos Pierna <pierna_f@hotmail.com>
2025-06-04 18:02:01 -0300

    merge groupby  4/06/2025

---
+++ pythonpandas/merge_groupby.py
@@ -0,0 +1,76 @@
+import pandas as pd
+
+#Abre o arquivo de Groupby como DataFrame
+vendas_DF = pd.read_excel("C:\\python_projetos\\python_rpa_projetos\\pythonpandas\\exibicao\\Groupby.xlsx")
+
+#Imprimindo os dados
+print(vendas_DF)
+
+print('*'*50)
+print('*'*50)
+print('*'*50)
+#mean = Média
+#groupby = Agrupando
+#mediaVendedor = vendas_DF.groupby(["Vendedor"]).mean()
+mediaVendedor = vendas_DF.groupby("Vendedor")["Total Vendas"].mean()
+
+#Imprimindo os dados
+print(mediaVendedor)
+
+print('*'*50)
+print('*'*50)   
+print('*'*50)
+
+#sum = Soma
+#groupby = Agrupando
+#somaVendedor = vendas_DF.groupby(["Vendedor"]).sum()
+somaVendedor = vendas_DF.groupby(["Vendedor"])["Total Vendas"].sum()
+
+
+#Imprimindo os dados
+print(somaVendedor)
+
+print('*'*50)
+print('*'*50)   
+print('*'*50)
+
+#sum = Soma
+#groupby = Agrupando
+#dropna=False = Não deleto os valores em branco
+#deixandoValoresEmBranco = vendas_DF.groupby(["Vendedor"], dropna=False).sum()
+deixandoValoresEmBranco = vendas_DF.groupby(["Vendedor"], dropna=False)["Total Vendas"].sum()
+#Imprimindo os dados
+print(deixandoValoresEmBranco)
+print('*'*50)
+print('*'*50)           
+print('*'*50)
+#sum = Soma
+#groupby = Agrupando
+#dropna=False = Não deleto os valores em branco
+#agrupaDuasColunas = vendas_DF.groupby(["Vendedor", "Produto"]).sum()
+agrupaDuasColunas = vendas_DF.groupby(["Vendedor", "Produto"])["Total Vendas"].sum()
+#
+#Imprimindo os dados
+print(agrupaDuasColunas)
+
+print('*'*50)
+print('*'*50)   
+print('*'*50)
+
+#sum = Soma
+#groupby = Agrupando
+#dropna=False = Não deleto os valores em branco
+agrupaFrutasVendedor = vendas_DF.groupby(["Produto", "Vendedor"])["Total Vendas"].sum()
+
+#Imprimindo os dados
+print(agrupaFrutasVendedor)
+print('*'*50)
+print('*'*50)   
+print('*'*50)
+#sum = Soma
+#groupby = Agrupando
+#dropna=False = Não deleto os valores em branco
+agrupaDataVendedor = vendas_DF.groupby(["Data Venda", "Vendedor"])["Total Vendas"].sum()
+
+#Imprimindo os dados
+print(agrupaDataVendedor)
