5bd6eee08b9c6c417e2e4789e53d6b524f487fd8
Francisco Carlos Pierna <pierna_f@hotmail.com>
2026-02-24 19:59:20 -0300

    dicionario 3 níveis  23/02/2026

--- GEEK_UNIVERSITY/dicionario3niveis.py
+++ GEEK_UNIVERSITY/dicionario3niveis.py
@@ -24,4 +24,23 @@ try:
     #cargo_maria = empresa["TI"]["Desenvolvimento"]["Joao"]["cargo"]
 except KeyError as e:
     print(f"Erro capturado! A chave {e} não foi encontrada.")
+    
+    # lista
+
+nome = 'Mariosvaldo'
+
+# Método 1: Usando o construtor list() (O mais direto)
+lista_caracteres = list(nome)
+
+print("Método 1:", lista_caracteres)
+# Saída: ['M', 'a', 'r', 'i', 'o', 's', 'v', 'a', 'l', 'd', 'o']
+print()
+
+# Método 2: List Comprehension (Útil se quiser aplicar alguma lógica)
+lista_com_loop = [char for char in nome]
+print('metodo 2',lista_com_loop)
+print()
+# Exemplo Extra: Se quiser apenas os caracteres únicos (sem repetição)
+lista_unica = list(set(nome))
+print(" Metodo 3 Caracteres únicos:", lista_unica)
 
