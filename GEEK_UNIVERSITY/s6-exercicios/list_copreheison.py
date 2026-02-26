173cf24e1320fa1005a83c83ca31372f7830857a
Francisco Carlos Pierna <pierna_f@hotmail.com>
2026-02-26 15:46:23 -0300

    list_copreheison.py  26/02/2026

--- GEEK_UNIVERSITY/s6-exercicios/list_copreheison.py
+++ GEEK_UNIVERSITY/s6-exercicios/list_copreheison.py
@@ -14,3 +14,19 @@ print('resultado_indireto ',resultado_indireto  )
 
 resultado_indireto = [p.capitalize() for p in palavra.split()]
 print('resultado_indireto ',resultado_indireto  ) 
+
+print()
+print()
+lista = [[1,2,3], [4,5,6], [7,8,9]]
+print('[0] [1] [2]', lista[0], lista[1], lista[2])
+print()
+print()
+for numero in lista:
+    print(numero)
+    print()
+    for l in numero:
+        print(l)
+print()
+print()
+lista1 = [numero for numero in lista   for l in numero] 
+print(lista1)       
